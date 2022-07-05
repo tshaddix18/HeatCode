46f5e5612e5cb529d50ce148539961f770a94b6a
tshaddix18 <71991863+tshaddix18@users.noreply.github.com>
2022-07-05 16:49:09 -0700

    tried taking a textbox and running it, not working so well

--- dockerwork/Filetesting.py
+++ dockerwork/Filetesting.py
@@ -1,36 +1,19 @@
 
 import subprocess
-#import docker
+
 
 image = "Dockerfile"
-
-#client = docker.from_env()
-#client.containers.run(image,  sys.argv[1], True)
-
-#client.images.build("dockerbuild-python-docker")
-
-#docker build --tag dockerbuild-python-docker .
-
-#docker run --publish 5000:5000 dockerbuild-python-docker
-
-#docker image rm -f dockerbuild-python-docker
-#subprocess.run(["cd dockerwork", "-l"])
-
 
 def run_DOCKER():
     print("well i guess this is a start")
     subprocess.run("docker build --tag dockerbuild-python-docker .",shell=True)
-    output =subprocess.run("docker run dockerbuild-python-docker ",shell=True,capture_output=True).stdout
-    print(type(output))
+    output = subprocess.run("docker run dockerbuild-python-docker ",shell=True,capture_output=True).stdout
+    
     with open("src/output.txt",'wb') as file:
         
             file.write(output)
-
-    
-    print("updating usercode")
-
     #output = subprocess.run("pwd", capture_output=True).stdout
-    subprocess.run(" docker logs dockerbuild-python-docker >& logs/myFile.txt",shell=True)
+    
     subprocess.run(" docker container ls -l",shell=True)
     #"docker inspect --format '{{join .Args " , "}}' container"
     subprocess.run("docker image rm -f dockerbuild-python-docker",shell=True)
