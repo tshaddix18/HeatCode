cd8c2a9840c1e9edefae69f7652e3d6a2a111f0c
eights <oliviaawongg@gmail.com>
2022-07-06 18:36:43 -0700

    Update README, add probems navbar and title card

--- heat-code/src/App.js
+++ heat-code/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  Nav,
+  Collapse,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+const WelcomeCard = () => {
+  return (
+    <div>
+      <Card outline body>
+        <CardBody>
+          <CardTitle tag="h5">Welcome to HeatCode☄️</CardTitle>
+          <CardText>
+            We're here to help make your interview grind process more
+            competitive and enjoyable. Select a problem to solve while competing
+            head-to-head with someone.
+          </CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+const Navigation = () => {
+  return (
+    <div>
+      <Navbar color="light" expand="md" light>
+        <NavbarBrand href="/">Problems</NavbarBrand>
+        <NavbarToggler onClick={function noRefCheck() {}} />
+        <Collapse navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/login/">Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/tshaddix18/HeatCode">
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Heat Code
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <WelcomeCard />
+      <p>Welcome to Heat Code</p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
