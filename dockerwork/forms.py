ce1474a3f9354de03b0625b36f5aa002a39a4dd4
tshaddix18 <71991863+tshaddix18@users.noreply.github.com>
2022-07-10 10:29:22 -0700

    Delete forms.py

--- dockerwork/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, PasswordField, SubmitField
-from wtforms.validators import  InputRequired,Length, EqualTo
-
-class registerform(FlaskForm):
-    username = TextAreaField('Username',validators=[InputRequired()])
-    Submit = SubmitField('Sign up')
