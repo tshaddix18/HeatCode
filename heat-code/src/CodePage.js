f762af2e564319e1d8a9b5bb06986deb1fb1175d
eights <oliviaawongg@gmail.com>
2022-07-11 17:36:24 -0700

    CodeMirror working

--- heat-code/src/CodePage.js
+++ heat-code/src/CodePage.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { Button } from "reactstrap";
-console.log("code page");
+import { okaidia } from "@uiw/codemirror-theme-okaidia";
+import CodeMirror from "@uiw/react-codemirror";
+import { python } from "@codemirror/lang-python";
+// https://uiwjs.github.io/react-codemirror/
 export const CodePage = () => {
-  console.log("code page comp");
   return (
     <>
       <h1>Code</h1>
-      {/* TODO: set onClick to connect with flask and run the docker tests somehow */}
-      {/*<Button color="primary">Run code</Button>*/}
+      <CodeMirror
+        value="print('hello world!')"
+        height="300px"
+        theme={okaidia}
+        extensions={[python()]}
+      />
+
+      {/* TODO: set onClick to connect with flask and run the docker tests somehow 
+      <Button
+        color="primary"
+        onClick={() => {
+          console.log("Print some text");
+        }}
+      >
+        Run code
+      </Button>*/}
     </>
   );
 };
