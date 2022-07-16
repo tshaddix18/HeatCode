da3a962f718eb1b9c5fe0b4bf8a838f572564525
Isha Manglik <108499810+imanglik@users.noreply.github.com>
2022-07-16 14:11:17 -0700

    test file that can run user code

--- test.py
+++ test.py
@@ -1,19 +1,111 @@
-import requests
+import sys
+from inspect import getmembers, isfunction
+from urllib.parse import uses_relative
+from userCode import *
 
-#Base URL
-BASE = "http://127.0.0.1:5000/"
+functions_list = getmembers(sys.modules['userCode'], isfunction)
+one,two,three,four,five,six = False, False, False, False, False, False
+output1, output2, output3, output4, output5 = 0, 0, 0, 0, 0
 
-response = requests.patch(BASE + "video/2", {"views":99})
-# data = [{"likes": 10, "name": "Alex", "views": 1},
-#         {"likes": 73, "name": "a", "views": 42},
-#         {"likes": 2, "name": "z", "views": 5}]
 
-# for i in range(len(data)):
-#     response = requests.put(BASE + "video/" + str(i), data[i])
-#     print(response.json())
-# #Test response with /helloworld
-# # response = requests.delete(BASE + "video/0")
-# # print(response)
-# input()
-response = requests.get(BASE + "video/6")
-print(response.json())
+def userCode(input):
+   temp = str()
+   functionName = str()
+
+   for i in functions_list:
+    for j in i:
+        functionName = j
+        break
+    break
+
+   x = eval(str(functionName) + '(input)')
+
+   return x
+
+def problemOne():
+    global output1
+    global output2
+    global output3
+    global output4
+    global output5
+
+    output1 = userCode("abcabcbb")
+    print(output1)
+    output2 = userCode("bbbbb")
+    output3 = userCode("pwwkew")
+    output4 = userCode("accovottw")
+    output5 = userCode("yywercrllweo")
+
+    outputs = True
+  
+    
+def problemTwo():
+    global output1
+    global output2
+    global output3
+    global output4
+    global output5
+
+    output1 = userCode([3,0,1])
+    output2 = userCode([0,1])
+    output3 = userCode([9,6,4,2,3,5,7,0,1])
+    output4 = userCode([1,4,5,6,2,3])
+    output5 = userCode([0,3,7,5,1,4,2])
+
+    outputs = True
+     
+
+def problemThree():
+    global output1
+    global output2
+    global output3
+    global output4
+    global output5
+
+    output1 = userCode(["i","love","leetcode","i","love","coding"], k = 2)
+    output2 = userCode(["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4)
+    output3 = userCode(["i", "like", "pizza", "i", "pizza"], k = 2)
+    output4 = userCode(["one","bright","sunny","sunny","day","one"], k = 2)
+    output5 = userCode(["under","the","weather","under","the","weather"], k = 3)
+
+    outputs = True
+
+def problemFour():
+    global output1
+    global output2
+    global output3
+   
+    output1 = userCode([1,2,3])
+    output2 = userCode([0])
+    output3 = userCode([2,3])
+
+    outputs = False
+   
+def problemFive():
+    global output1
+    global output2
+    global output3
+
+    output1 = userCode(s = "aa", p = "a")
+    output2 = userCode(s = "aa", p = "a*")
+    output3 = userCode(s = "ab", p = ".*")
+
+    outputs = False
+    
+def problemSix():
+    global output1
+    global output2
+    global output3
+
+    output1 = userCode(s = "(()")
+    output2 = userCode(s = ")()())")
+    output3 = userCode(s = ")()()())")
+
+    outputs = False
+   
+# if the user is solving problem one
+problemOne()
+one = True
+
+
+
