a35c71cffa7468916aa128c7cd9c5a64e12b4ba9
tshaddix18 <71991863+tshaddix18@users.noreply.github.com>
2022-07-16 19:00:11 -0700

    tried to make js work

--- heat-code/src/CodePage.js
+++ heat-code/src/CodePage.js
@@ -1,8 +1,11 @@
 import React,{useState,useEffect} from "react";
+import { readFileSync } from "fs";
+const fs = require('fs')
 import { Table, Card, CardBody, CardTitle, CardText, Button } from "reactstrap";
 import { okaidia } from "@uiw/codemirror-theme-okaidia";
 import CodeMirror from "@uiw/react-codemirror";
 import { python } from "@codemirror/lang-python";
+
 // https://uiwjs.github.io/react-codemirror/
 // https://stackoverflow.com/questions/57024486/react-get-codemirror-value-onclick
 
@@ -31,7 +34,7 @@ const CodeCard = () => {
   );
 };
 export const CodePage = () => {
-
+  const fs = require('fs');
   const [data, setData] = useState({data: []});
   const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState('');
@@ -78,7 +81,11 @@ export const CodePage = () => {
 
       <div class="m-4">
         <button class="btn btn-outline-dark btn-lg" onClick={handleClick}>
-          {" "}
+          {fs.writeFile('Otpt.txt', data, (err) => {
+      
+      // In case of a error throw err.
+      if (err) throw err;
+  })}
 
           Run code
         </button>
