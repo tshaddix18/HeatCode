4ce57b310c0c2c44515e58b03e5b8acc2272e464
Alexander Chen <achen78@ucsc.edu>
2022-07-18 16:59:28 -0700

    savetime and printtime

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, render_template, url_for, flash, request
+from datetime import datetime, timedelta
+import time
+import sys
+sys.path.append('../')
+import Filetesting
+
+
+app = Flask(__name__)
+@app.route('/time')
+def getcurrenttime():
+    return {'time':time.time()}
+
+
+@app.route('/docker')
+def getnewcurrenttime():
+    Filetesting.run_DOCKER()
+    #with open "dockerwork/src/" as file:
+    text_file = open("dockerwork/src/output.txt", "r")
+    data = text_file.read()
+    text_file.close()
+    print((str("hi")))
+    #print(jsonify(str(data)))
+   # print(type(jsonify(str(data))))
+    return jsonify(str(data))
+
+@app.route("/senduserdata", methods=["POST"], strict_slashes=False)
+def add_articles():
+    data = request.get_json()
+    with open("dockerwork/src/usercode.txt","w") as file:
+        file.write(data["userCode"])
+        #print("the data is "+ str(data["userCode"]))
+        
+
+    return "success"
+@app.route("/problem", methods=["POST"], strict_slashes=False)
+def add_problem():
+    data = request.get_json()
+    print(type(data))
+    print(str(data))
+    with open("dockerwork/src/problemid.txt","w") as file:
+        file.write(str(data["problemId"]))
+        
+       
+        
+
+    return "success"
+
+
+@app.route("/savetime", methods=["POST"], strict_slashes=False)
+def save_time():
+    data = time.time()
+    with open("dockerwork/src/time.txt","w") as file:
+        file.write(data["time"])
+
+    return "success"
+
+@app.route("/printtime")
+def print_time():
+    text_file = open("dockerwork/src/time.txt", "r")
+    data = text_file.read()
+    text_file.close()
+    new_time = (time.time() - data).totalminutes()
+    
+    return jsonify(str(new_time))
