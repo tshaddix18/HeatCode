c238b921ef197cb56a607126654254fd07debfaa
Isha Manglik <108499810+imanglik@users.noreply.github.com>
2022-07-18 00:53:11 -0700

    updated test cases

--- main.py
+++ main.py
@@ -1,97 +1,26 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
-
-class VideoModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    views = db.Column(db.Integer, nullable=False)
-    likes = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"Video(name={name}, views={views}, likes={likes}"
-
-#only use once
-#db.create_all()
-
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
-video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
-video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
-
-# videos = {}
-
-video_update_args = reqparse.RequestParser()
-video_update_args.add_argument("name", type=str, help="Name of the video")
-video_update_args.add_argument("views", type=int, help="Views of the video")
-video_update_args.add_argument("likes", type=int, help="Likes on the video")
-
-resource_fields = {
-    'id': fields.Integer,
-    'name': fields.String,
-    'views': fields.Integer,
-    'likes': fields.Integer
-}
-
-def abort_if_notexist(video_id):
-    if video_id not in videos:
-        abort(404, message="Video id is not valid")
-
-def abort_if_exists(video_id):
-    if video_id in videos:
-        abort(409, message="Video already exists")
-
-class Video(Resource):
-    @marshal_with(resource_fields)
-    def get(self, video_id):
-        result = VideoModel.query.filter_by(id=video_id).first()
-        if not result:
-            abort(404, message="Could not find video with that id")
-        return 
-
-    @marshal_with(resource_fields)
-    def put(self, video_id):
-        args = video_put_args.parse_args()
-        result = VideoModel.query.filter_by(id=video_id).first()
-        if result:
-            abort(409, message="Video id taken")
-        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
-        db.session.add(video)
-        db.session.commit()
-        return video, 201
-
-    @marshal_with(resource_fields)
-    def patch(self, video_id):
-        args = video_update_args.parse_args()
-        result = VideoModel.query.filter_by(id=video_id).first()
-        if not result:
-            abort(404, message="Video doesn't exist")
-        if args['name']:
-            result.name = args['name']
-        if args['views']:
-            result.views = args['views']
-        if args['likes']:
-            result.likes = args['likes']
-
-        db.session.commit()
-        return result
-
-    def delete(self, video_id):
-        abort_if_notexist(video_id)
-        del videos[video_id]
-        return '', 204
+import sys
+#import subprocess
 
 
-#adds hello world as a resource at url /helloworld
-api.add_resource(Video, "/video/<int:video_id>")
+problemID = sys.argv[1]
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if problemID == "1":
+    exec(open('problemOne.py').read())
+   
+if problemID == "2":
+    exec(open('problemTwo.py').read())
+
+if problemID == "3":
+    exec(open('problemThree.py').read())
+   
+if problemID == "4":
+    exec(open('problemFour.py').read())
+
+if problemID == "5":
+    exec(open('problemFive.py').read())
+   
+if problemID == "6":
+    exec(open('problemSix.py').read())
 
 
 
